d4356506aab73cad3e877101f3e871e4cf7076db
Pupis <pupelis.justas@gmail.com>
2020-12-06 22:37:11 +0200

    first: second part

--- first.js
+++ first.js
@@ -1,11 +1,16 @@
 const fs = require('fs');
 
 let data = fs.readFileSync('./fixtures/first.txt').toString().split('\n');
-let i, j;
+
 for (let i = 0; i < data.length; i++) {
     for (let j = i + 1; j < data.length; j++) {
         if ((parseInt(data[i]) + parseInt(data[j])) === 2020) {
             console.log('Result is ' + (parseInt(data[i]) * parseInt(data[j])));
+        }
+        for (let k = j + 1; k < data.length; k++) {
+            if ((parseInt(data[i]) + parseInt(data[j]) + parseInt(data[k])) === 2020) {
+                console.log('Result is ' + (parseInt(data[i]) * parseInt(data[j]) * parseInt(data[k])));
+            }
         }
     }
 }
